8c4c31fd7266d5eda16ac226f50e6c682113f822
sandip662 <sandipmondal9748@gmail.com>
2025-03-26 12:37:16 +0530

    commit Match password

---
+++ Bloggie.Web/Controllers/EncryptionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class EncryptionController : Controller
+    {
+        private readonly PasswordHasher<string> _passwordHasher;
+
+        public EncryptionController()
+        {
+            _passwordHasher = new PasswordHasher<string>();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VerifyPassword(string password, string hash)
+        {
+            var result = _passwordHasher.VerifyHashedPassword(null, hash, password);
+
+            if (result == PasswordVerificationResult.Success)
+            {
+                ViewBag.SwalMessage = "Password is correct!";
+                ViewBag.SwalIcon = "success";
+            }
+            else
+            {
+                ViewBag.SwalMessage = "Invalid password!";
+                ViewBag.SwalIcon = "error";
+            }
+
+            return View("Index");
+        }
+    }
+}
